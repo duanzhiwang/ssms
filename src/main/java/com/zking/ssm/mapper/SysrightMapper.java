6e01dc24795b427cad58715c811736aa647ead78
duanzhiwang <2697019085@qq.com>
2020-12-01 08:22:21 +0800

    ssm项目

--- src/main/java/com/zking/ssm/mapper/SysrightMapper.java
+++ src/main/java/com/zking/ssm/mapper/SysrightMapper.java
@@ -3,13 +3,13 @@ package com.zking.ssm.mapper;
 import com.zking.ssm.model.Sysright;
 
 public interface SysrightMapper {
-    int deleteByPrimaryKey(String rightCode);
+    int deleteByPrimaryKey(Integer perid);
 
     int insert(Sysright record);
 
     int insertSelective(Sysright record);
 
-    Sysright selectByPrimaryKey(String rightCode);
+    Sysright selectByPrimaryKey(Integer perid);
 
     int updateByPrimaryKeySelective(Sysright record);
 
